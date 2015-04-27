b9eef61fda72a4f3d5f54bb57ac65a87d5f4f341
Amanda Peterson <amandaleighp@gmail.com>
2015-04-26 19:02:25 -0700

    Completed all responsive for entire page

--- js/beepi.js
+++ js/beepi.js
@@ -1,20 +1,49 @@
 $(document).ready(function(){
-	$('.money').on('click', function() {
-		// var toggleText = 
-		// $('.active').removeClass('active');
-		$('.time').removeClass('active');
-	  $('.money').addClass('active');
-	  $('.banner1').addClass('banner1-toggle');
-	  $('.time-text').addClass('no-display');
-	  $('.money-text').removeClass('no-display');
+	
+	$('.btn').on('click', function() {
+		if ($(this).hasClass('money')) {
+			$(this).addClass('active');
+			$('.time').removeClass('active');
+			$('.bcg-1').addClass('banner1-toggle');
+			$('.time-text').addClass('no-display');
+			$('.money-text').removeClass('no-display');
+		} else if ($('.btn').hasClass('time')) {
+			$(this).addClass('active');
+			$('.money').removeClass('active');
+			$('.bcg-1').removeClass('banner1-toggle');
+			$('.money-text').addClass('no-display');
+			$('.time-text').removeClass('no-display');
+		}
 	});
-	$('.time').on('click', function() {
-		// var toggleText = 
-		// $('.active').removeClass('active');
-		$('.money').removeClass('active');
-	  $('.time').addClass('active');
-	  $('.banner1').removeClass('banner1-toggle');
-	  $('.money-text').addClass('no-display');
-	  $('.time-text').removeClass('no-display');
-	});
+
+	function adjustWindow(){
+		
+		// Init Skrollr
+		var s = skrollr.init();
+		
+		// Get window size
+	    winH = $window.height();
+	    
+	    // Keep minimum height 550
+	    if(winH <= 550) {
+			winH = 550;
+		} 
+	    
+	    // Resize our slides
+	    $slide.height(winH);
+	    
+	    // Refresh Skrollr after resizing our sections
+	    s.refresh($('.homeSlide'));
+	    
+	}
 });
+
+// $("#menuToggle").click(function () {
+//     $('.wrapper,header,nav').toggleClass('push');
+
+//         if ($('.wrapper,header,nav').hasClass('push')) {
+//             $('.wrapper').css('position', 'fixed');
+//         } else {
+//             $('.wrapper').css('position', 'relative');
+//         }
+
