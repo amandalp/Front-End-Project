a58198124e4c459b08f5cc569f25eedd862e64b1
Amanda Peterson <amandaleighp@gmail.com>
2015-04-27 01:22:25 -0700

    Have parallax working on desktop sized screens and smaller but not on bigger screens

--- js/beepi.js
+++ js/beepi.js
@@ -16,27 +16,73 @@ $(document).ready(function(){
 		}
 	});
 
-	function adjustWindow(){
-		
-		// Init Skrollr
-		var s = skrollr.init();
-		
-		// Get window size
-	    winH = $window.height();
-	    
-	    // Keep minimum height 550
-	    if(winH <= 550) {
-			winH = 550;
-		} 
-	    
-	    // Resize our slides
-	    $slide.height(winH);
-	    
-	    // Refresh Skrollr after resizing our sections
-	    s.refresh($('.homeSlide'));
-	    
+
+	$.fn.parallax = function(options) {
+	 
+    var windowHeight = $(window).height();
+
+    // Establish default settings
+    var settings = $.extend({
+        speed : 0.15
+    }, options);
+
+    // Iterate over each object in collection
+    return this.each( function() {
+
+    	// Save a reference to the element
+    	var $this = $(this);
+
+    	// Set up Scroll Handler
+    	$(document).scroll(function(){
+	 
+        var scrollTop = $(window).scrollTop();
+        var offset = $this.offset().top;
+        var height = $this.outerHeight();
+	 
+    	// Check if above or below viewport
+			if (offset + height <= scrollTop || offset >= scrollTop + windowHeight) {
+				return;
+			}
+	 
+			var yBgPosition = Math.round((offset - scrollTop) * settings.speed);
+	 
+        // Apply the Y Background Position to Set the Parallax Effect
+  			$this.css('background-position', 'center ' + yBgPosition + 'px');
+	                
+	    });
+	  });
 	}
+}(jQuery));
+
+$('.bcg-1,.bcg-2').parallax({
+	speed :	0.01
 });
+
+$('.bcg-1,.bcg-2').parallax({
+	speed :	0.15
+});
+
+// 	function adjustWindow(){
+		
+// 		// Init Skrollr
+// 		var s = skrollr.init();
+		
+// 		// Get window size
+// 	    winH = $window.height();
+	    
+// 	    // Keep minimum height 550
+// 	    if(winH <= 550) {
+// 			winH = 550;
+// 		} 
+	    
+// 	    // Resize our slides
+// 	    $slide.height(winH);
+	    
+// 	    // Refresh Skrollr after resizing our sections
+// 	    s.refresh($('.homeSlide'));
+	    
+// 	}
+// });
 
 // $("#menuToggle").click(function () {
 //     $('.wrapper,header,nav').toggleClass('push');
